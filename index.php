3519403ee3aeeeb0d2aa998b2dfb594a7f462687
Arslan Ahsan Jalib <101581096+Arslan-Ahsan-Jalib-Dev@users.noreply.github.com>
2022-03-15 21:33:46 +0500

    smooth scroll with structure and styles included

---
+++ index.php
@@ -0,0 +1,114 @@
+<html lang="en">
+<head>
+  <title>Smooth Scroll</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel ="stylesheet" href="styles.css">
+</head>
+<body>
+  
+  <section class="simple_nav">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                      <a class="navbar-brand" href="#">Smooth Scroll</a>
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mx-auto">
+                          <li class="nav-item active">
+                            <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#stats">Stats</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#testimonials">Testimonials</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                          </li>
+                        </ul>
+                       
+                      </div>
+                    </nav>
+                  
+                  
+              </div>
+          </div>
+      </div>
+      
+  </section>
+  
+  <section class="about_area common_area" id="about">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="sec_top text-center">
+                       <h2>
+                           This is About Section
+                       </h2>
+                   </div>
+               </div>
+           </div>
+       </div>
+  </section>
+  
+  <section class="stats_area common_area" id="stats">
+        <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="sec_top text-center">
+                       <h2>
+                           This is Stats Section
+                       </h2>
+                   </div>
+               </div>
+           </div>
+       </div>
+  </section>
+  
+  <section class="testimonials_area common_area" id="testimonials">
+    <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="sec_top text-center">
+                       <h2>
+                           This is Testimonials Section
+                       </h2>
+                   </div>
+               </div>
+           </div>
+       </div>
+  </section>
+  
+  <section class="contact_area common_area" id="contact">
+    <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="sec_top text-center">
+                       <h2>
+                           This is Contact Section
+                       </h2>
+                   </div>
+               </div>
+           </div>
+       </div>
+  </section>
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./script.js"></script>
+
+</body>
+</html>
